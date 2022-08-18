e332b0caf36a8103d7d29f827f0149281743ac54
Baga9898 <61385284+Baga9898@users.noreply.github.com>
2022-08-18 23:44:13 +0300

    Add create resource route

---
+++ routes/resources.routes.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import Resource from "../models/Resource";
+
+const router = new Router();
+
+router.post('/resources', async(req, res) => {
+    try {
+        const { name, link, description, createDate, modifiedDates, categories } = req.body;
+        const resource = new Resource({ name, link, description, createDate, modifiedDates, categories });
+        await resource.save();
+        return res.json(resource);
+    } catch (error) {
+        res.json(error);
+    }
+})
+
+export default router;
