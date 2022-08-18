55666e9d148ac4b575d386153663bb9cbc892954
Baga9898 <61385284+Baga9898@users.noreply.github.com>
2022-08-19 00:13:58 +0300

    Add Get all resources with category filter

--- routes/resources.routes.js
+++ routes/resources.routes.js
@@ -2,13 +2,28 @@ import Router from "express";
 import Resource from "../models/Resource.js";
 
 const router = new Router();
+const basePath = '/resources';
 
-router.post('/resources', async(req, res) => {
+router.post(basePath, async(req, res) => {
     try {
         const { name, link, description, createDate, modifiedDates, categories } = req.body;
         const resource = new Resource({ name, link, description, createDate, modifiedDates, categories });
         await resource.save();
         return res.json(resource);
+    } catch (error) {
+        res.json(error);
+    }
+})
+
+router.get(basePath, async(req, res) => {
+    try {
+        const query = req.query;
+        let resources;
+
+        query.hasOwnProperty('category') ?
+            resources = await Resource.find(query) : resources = await Resource.find();
+
+        return res.json(resources);
     } catch (error) {
         res.json(error);
     }
