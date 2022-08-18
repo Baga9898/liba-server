45806bc350e20de905c8f1e3dfd48254631728e2
Baga9898 <61385284+Baga9898@users.noreply.github.com>
2022-08-18 23:12:11 +0300

    Add DB connect

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ import express from "express";
 import mongoose from "mongoose";
 import config from 'config';
 
-
 const app = express();
 const PORT = config.get('serverPort') || 5000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('yep');
-})
+const startServer = async() => {
+    try {
+        mongoose.connect(config.get('DBUrl'));
 
-app.listen(PORT, () => console.log(`SERVER STARTED ON ${PORT} PORT`));
+        app.listen(PORT, () => {
+            console.log(`SERVER STARTED ON ${PORT} PORT`)
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+startServer();
