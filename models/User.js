d33607e4c39337d4f137622a41efaab64bc5ba1b
Baga9898 <61385284+Baga9898@users.noreply.github.com>
2022-08-21 18:38:05 +0300

    Registration is ready

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const User = new mongoose.Schema({
-    name: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     roles: [{ type: String, ref: 'Role' }]
 })
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,34 @@
 import { Router } from "express";
+import { check, validationResult } from "express-validator";
+import bcrypt from 'bcryptjs';
+
 import User from "../models/User.js";
 import Role from "../models/Role.js";
 
 const router = new Router();
 
-router.post('/registration', (req, res) => {
-
+router.post('/registration', [
+        check('username', 'Cannot be empty').notEmpty(),
+        check('password', 'Password must be longer than 3 characters').isLength({ min: 3 }),
+    ], async(req, res) => {
+    try {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).json({ message: 'Registration error', errors });
+        }
+        const { username, password } = req.body;
+        const candidate = await User.findOne({ username });
+        if (candidate) {
+            return res.status(400).json({ message: 'User is exist' })
+        };
+        const hashedPassword = bcrypt.hashSync(password, 7);
+        const userRole = await Role.findOne({ name: 'watcher' });
+        const user = new User({ username, password: hashedPassword, roles: [userRole.name] });
+        user.save();
+        return res.json({ message: 'User has been successfully registered' })
+    } catch (error) {
+        console.error(error);
+    }
 })
 
 router.post('/login', (req, res) => {
